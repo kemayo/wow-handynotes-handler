9300c125576f32b3f77b01067e68c39575114de5
David Lynch <kemayo@gmail.com>
2025-12-28 15:01:39 -0600

    Switch to tooltip scanning for decor completion for now
    It's less elegant, but I feel like forcing the catalog-load isn't ideal

--- rewards/item.lua
+++ rewards/item.lua
@@ -407,29 +407,18 @@ function ns.rewards.Decor:Obtained(...)
         return true
     end
     if not C_HousingCatalog then return GetItemCount(self.id, true) > 0 end
-    -- This only works because of the call in Cache; the alternative would be
-    -- tooltip-scanning, but that's not ideal because there's not a type for
-    -- the quantity line so it'd be falling back on some localized text...
-    local decorInfo = C_HousingCatalog.GetCatalogEntryInfoByItem(self.id, true)
-    if decorInfo then
-        return (decorInfo.quantity + decorInfo.remainingRedeemable + decorInfo.numPlaced) >= 1
+    local pattern = HOUSING_DECOR_OWNED_COUNT_FORMAT:gsub("([%(%)])", "%%%1"):gsub("%%d", "(%%d+)")
+    local info = C_TooltipInfo.GetItemByID(self.id)
+    if info then
+        for _, line in ipairs(info.lines) do
+            if line.type == Enum.TooltipDataLineType.None and line.leftText and string.match(line.leftText, pattern) then
+                return true
+            end
+        end
+        return false
     end
 end
 function ns.rewards.Decor:Notable(...)
-    -- could only count xp-granting as notable? firstAcquisitionBonus on decorInfo
+    -- could only count xp-granting as notable? firstAcquisitionBonus on C_HousingCatalog.GetCatalogEntryInfoByItem
     return ns.db.decor_notable and self:super("Notable", ...)
-end
-do
-    local catalogLoaded
-    function ns.rewards.Decor:Cache()
-        self:super("Cache")
-        if not catalogLoaded then
-            -- Everything about decor *but* the quantity you possess is
-            -- available at all times, but this needs to be called to fetch
-            -- quantities:
-            C_HousingCatalog.CreateCatalogSearcher()
-            catalogLoaded = true
-        end
-        C_HousingCatalog.GetCatalogEntryInfoByItem(self.id, true)
-    end
 end
