c7e6fb129b7fe400ee6f31f9ba02b3f5a4a83a9a
David Lynch <kemayo@gmail.com>
2026-02-23 22:39:15 -0600

    Override reward item tooltip display to not use a MoneyFrame

--- rewards/item.lua
+++ rewards/item.lua
@@ -80,8 +80,42 @@ function ns.rewards.Item:MightDrop()
     -- parent catches covenants / classes / etc
     return self:super("MightDrop")
 end
-function ns.rewards.Item:SetTooltip(tooltip)
-    tooltip:SetItemByID(self.id)
+do
+    local function copper_to_pretty_money(c)
+        if c >= 10000 then
+            return ("|cffffffff%d|r|cffffd700%s|r|cffffffff%d|r|cffc7c7cf%s|r|cffffffff%d|r|cffeda55f%s|r"):format(
+                BreakUpLargeNumbers(c/10000), GOLD_AMOUNT_SYMBOL, (c/100)%100, SILVER_AMOUNT_SYMBOL, c%100, COPPER_AMOUNT_SYMBOL
+            )
+        elseif c >= 100 then
+            return ("|cffffffff%d|r|cffc7c7cf%s|r|cffffffff%d|r|cffeda55f%s|r"):format((c/100)%100, SILVER_AMOUNT_SYMBOL, c%100, COPPER_AMOUNT_SYMBOL)
+        else
+            return ("|cffffffff%d|r|cffeda55f%s|r"):format(c%100, COPPER_AMOUNT_SYMBOL)
+        end
+    end
+    local function cleanTooltipMoney(tooltip, lineData)
+        -- see: TooltipDataRules.SellPrice and GameTooltip_OnTooltipAddMoney
+        if lineData.type == Enum.TooltipDataLineType.SellPrice and lineData.price then
+            if lineData.maxPrice and lineData.maxPrice >= 1 then
+                GameTooltip_AddColoredLine(tooltip, ("%s:"):format(SELL_PRICE), HIGHLIGHT_FONT_COLOR)
+                local indent = string.rep(" ", 4)
+                GameTooltip_AddHighlightLine(tooltip, string.format("%s%s", MINIMUM, copper_to_pretty_money(lineData.price)))
+                GameTooltip_AddHighlightLine(tooltip, string.format("%s%s", MAXIMUM, copper_to_pretty_money(lineData.maxPrice)))
+            else
+                GameTooltip_AddHighlightLine(tooltip, string.format("%s: %s", SELL_PRICE, copper_to_pretty_money(lineData.price)))
+            end
+            return true
+        end
+    end
+    function ns.rewards.Item:SetTooltip(tooltip)
+        if C_TooltipInfo then
+            -- secrets are enough of a problem that I feel this has become necessary...
+            local tooltipInfo = CreateBaseTooltipInfo("GetItemByID", self.id)
+            tooltipInfo.linePreCall = cleanTooltipMoney
+            tooltip:ProcessInfo(tooltipInfo)
+            return
+        end
+        tooltip:SetItemByID(self.id)
+    end
 end
 function ns.rewards.Item:AddToItemButton(button)
     button:SetItem(self.id)
